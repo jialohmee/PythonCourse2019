5786ad0a87d6a605acf97770a7ebbea06262601e
Jeremy Siow <wsiow@wustl.edu>
2019-08-14 21:18:21 -0500

    Homework 2 Jeremy
    PENULTIMATE VERSION

--- homeworks/hw2/hw2_jeremysiow.py
+++ homeworks/hw2/hw2_jeremysiow.py
@@ -10,114 +10,45 @@ from bs4 import BeautifulSoup
 import urllib.request
 import requests
 import csv 
+import re
 
-url = "https://petitions.whitehouse.gov"
-page = urllib.request.urlopen(url)
-html = BeautifulSoup(page.read(), "html.parser")
-
-for petition in range(len(html.find_all("h3"))):
-	try:
-		petition = {}
-		petition_url = url + html.find_all("h3")[petition].a["href"]
-		petition_page = urllib.request.urlopen(petition_url)
-		petition_html = BeautifulSoup(petition_page.read(), "html.parser")		
-	except:
-		pass
-
-petition["Title"] = petition_html.find("h1").text
-petition["Date"] = petition_html.find("h4").text
-petition["Signatures"] = petition_html.find("div", {"class" : "signatures-text-container"}).text
-
-for i in petition_html.find_all("div", {"class" : "field-item odd"}):
-	print(i)
-
-
-
-for i in petitions:
-	print(i)
-	
-html.find_all("h3")[0].a["href"]
-
-
-
-
-extension = html.find("div", {"class": "page-load-next"}) # find "load more"
-text = extension.text
-
-urls = [url]
-
-while text is not None:
-	urls.append(url + extension.a["href"])
-	new_url = url + extension.a["href"]
-	new_page = urllib.request.urlopen(base_url)
-	html = BeautifulSoup(page.read(), "html.parser")
-	extension = html.find("div", {"class": "page-load-next"})
-	text = extension.text
-
-
-
-text = extension.text
-href = extension.a["href"]
-urls = [url]
-urls.append(url + href)
-urllib.request.urlopen(urls[1])
-
-urls = [url]
-base_url = "https://petitions.whitehouse.gov/petitions"
-for i in range()
-
-
-while extension.text == "Load More":
-	urls.append(url + extension.a["href"])
-	base_url = url + extension.a["href"]
-	page = urllib.request.urlopen(base_url)
-	html = BeautifulSoup(page.read(), "html.parser")
-	extension = html.find("div", {"class": "page-load-next"})
-
-urls
-
-urls.append(url)
-urls.append(url + extension.a["href"])
-
-
-
-urls = []
-while extension.text == "Load More":
-	urls.append(base_url + extension.a["href"])
-	url = url + extension.a["href"]
-	page = urllib.request.urlopen(url)
-	html = BeautifulSoup(page.read(), "html.parser")
-	extension = html.find("div", {"class" : "page-load-next"})
-
-extension.text
-
-urls = [base_url]
-urls.append(base_url + base_extension.a["href"])
-urls
-
-base_extension.a["href"]
-
-with open('hw2_solution_jeremy.csv', 'w') as f:
-  w = csv.DictWriter(f, fieldnames = ("Title", "Published Date", "Issues", "Number of Signatures"))
-  w.writeheader()
-  base_url = "https://petitions.whitehouse.gov/petitions"
-  base_page = urllib.request.urlopen(base_url)
-  base_html = BeautifulSoup(base_page.read(), "html.parser")
-  base_extension = base_html.find("div", {"class" : "page-load-next"})
-  base_extension.a["href"]
-
-
-extensions = []
-
-
-url = "https://petitions.whitehouse.gov/petitions?page=4"
-page = urllib.request.urlopen(url)
-html = BeautifulSoup(page.read(), "html.parser")
-html.find("div", {"class" : "page-load-next"})
-
-url = "https://petitions.whitehouse.gov/petitions?page=3"
-page = urllib.request.urlopen(url)
-html = BeautifulSoup(page.read(), "html.parser")
-extension = html.find("div", {"class" : "page-load-next"})
-extension.a["href"]
-extension.text == "Load More"
+with open("petitions.csv", "w") as f:
+	w = csv.DictWriter(f, fieldnames = ("No", "Title", "Date", "Signatures", "Issue1", "Issue2", "Issue3")) # set headers for dataframe
+	w.writeheader() # write headers to csv file
+	base_url = "https://petitions.whitehouse.gov" # set base url which will be concatenated with respective href in the loop
+	extension = "" # set extension to null
+	number = 0 # set counter to 0
+	text = "Load More" # create "load more" text for while loop
+	while text == "Load More": 
+	# when "load more" is present in current page, performing following methods. if not, stop operation
+		url = base_url + extension # concatenate base_url with extension
+		print(url) # check if url is correct
+		page = urllib.request.urlopen(url) # open url
+		html = BeautifulSoup(page.read(), "html.parser") # save html
+		petitions = {} # empty dictionary to store keys and values from current url
+		for petition in range(len(html.find_all("h3"))): # for all h3 tags in current url	
+			try: # perform methods for those h3 tags that have <a href> values
+				petition_url = base_url + html.find_all("h3")[petition].a["href"] # save url for each petition
+				petition_page = urllib.request.urlopen(petition_url) # open petition url
+				petition_html = BeautifulSoup(petition_page.read(), "html.parser") # save petition html
+				number += 1 # add 1 to accumulator variable
+				petitions["No"] = number # store accumulator variable as "No" key in dict
+				petitions["Title"] = petition_html.find("h1").get_text() # store petition title as "Title" key in dict
+				date = petition_html.find("h4").get_text()
+				petitions["Date"] = date.split(" on ")[1]
+				#petitions["Date"] = petition_html.find("h4").text # store date created as "Date" key in dict
+				signatures = petition_html.find("div", {"class" : "signatures-text-container"}).text # store number of signatures as "Signatures" key in dict
+				signatures_pattern = re.compile(r"(?<=)\n")
+				petitions["Signatures"] = int(signatures_pattern.split(signatures)[0].replace(',', ''))
+				tags = petition_html.find("div", {"class" : "field field-name-field-petition-issues field-type-taxonomy-term-reference field-label-hidden tags"}).get_text()
+				tags_pattern = re.compile(r"(?<=[a-z])(?=[A-Z])")
+				for i in range(len(tags_pattern.split(tags))):
+					petitions["Issue" + str(i+1)] = tags_pattern.split(tags)[i]
+				print(petitions) # check if keys and values are correctly stored in dict
+				w.writerow(petitions) # write dict to csv file
+			except: 
+				continue # skips those h3 tags that have no <a href> values
+		text = html.find("div", {"class": "page-load-next"}).get_text() # updates looping variable "text". 
+		try: extension = html.find("div", {"class": "page-load-next"}).a["href"] # if "load more" is present, find and update extension
+		except TypeError: # if "load more" is not present in current url, stop operation
+			print("You have reached the end of the page") # print error message
